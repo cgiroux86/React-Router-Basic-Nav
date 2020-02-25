5575478997e8196788f33dfdb5dcb7c654610e70
Christopher Giroux <cgiroux86@gmail.com>
2020-02-25 12:01:13 -0500

    MVP plus stretch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {Route} from 'react-router-dom'
 
 const App = () => (
   <div>
-    <Navigation />
+    <Navigation>
+      <Route>
+         <Home exact path='/'/>
+      </Route>
+      <Route>
+        <About exact path-='/about'/>
+      </Route>
+      <Route>
+        <Contact exact path='/contact'/>
+      </Route>
+     </Navigation>
   </div>
 );
 
